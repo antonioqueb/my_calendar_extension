0497e22fa781aa102337c849c55c449217ab1ff3
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-07 19:25:28 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+# Archivo: my_calendar_extension/__manifest__.py
+
+{
+    'name': 'Calendar Event Company Extension',
+    'version': '1.0',
+    'depends': ['calendar', 'base'],
+    'data': [],
+    'installable': True,
+}
